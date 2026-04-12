3bc8ebbd403bec5bd670558bffb15085a964cdb1
RevenueClaw <revenueclaw@agentmail.to>
2026-04-11 22:34:22 -0400

    v1.11: rootDir './src', outDir './build' (build/server.js direct for Render); prisma generate first; clean TS/Prisma; .gitignore build/

--- backend/build/src/middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const middleware = () => {
-    return (req, res, next) => {
-        console.log(`${req.method} ${req.path} - ${req.ip}`);
-        next();
-    };
-};
-exports.default = middleware;
